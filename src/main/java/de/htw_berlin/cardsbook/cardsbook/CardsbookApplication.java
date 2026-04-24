471e5fc64c4c571ceb56196a43aa509ddcd56cdb
Deniz <s0571123@htw-berlin.de>
2026-04-24 11:00:51 +0200

    first commit

---
+++ src/main/java/de/htw_berlin/cardsbook/cardsbook/CardsbookApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.cardsbook.cardsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/cardsbook/cardsbook/CardsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.cardsbook.cardsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
